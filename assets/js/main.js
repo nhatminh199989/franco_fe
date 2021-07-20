f1aee1cc95a33d0a3102b39d3ed957115da0149f
nhatminh199989 <42006110+nhatminh199989@users.noreply.github.com>
2021-07-20 22:21:17 +0700

    Finish blog section

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(".owl-carousel").owlCarousel({
+  items: 3,
+  loop: true,
+  margin: 30,
+  dots:true,  
+  responsive: {
+    0: {
+      items: 1,
+    },
+    1140: {
+      items: 3,
+    },
+  },
+});
